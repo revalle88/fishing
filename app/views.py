d323b5a7df74a146a8c1e8c86a8644c0d8c50266
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-01-17 17:58:24 +0300

    app urls, view

--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def pound_list(request):
+    return render(request, 'app/pound_list.html', {})
