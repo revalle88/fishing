d4800995b9a240b806a06a69a18d94a1e973afd1
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-01-17 09:42:32 +0300

    Pound model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+class Pound(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.name
