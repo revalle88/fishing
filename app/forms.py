541f13f1fa3d3abd1d3a173904e51cd2d78eb366
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-01-18 09:45:01 +0300

    add pound form

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Pound
+
+
+class PoundForm(forms.ModelForm):
+
+    class Meta:
+        model = Pound
+        fields = ('name', 'description',)
--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Pound
+from .forms import PoundForm
+
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -13,3 +16,15 @@ def home(request):
 def pound_list(request):
     pounds = Pound.objects.all()
     return render(request, 'app/pound_list.html', {"pounds": pounds})
+
+def pound_new(request):
+    if request.method == "POST":
+        form = PoundForm(request.POST)
+        if form.is_valid():
+            pound = form.save(commit=False)
+            pound.author = request.user
+            pound.save()
+            return redirect('pounds')
+    else:
+        form = PoundForm()
+        return render(request, 'app/pound_edit.html', {'form': form})
