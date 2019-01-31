9b8f4d07f84d613020ffb5670c93aebf04712a57
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-01-31 10:37:06 +0300

    accounts login

--- fishing/settings.py
+++ fishing/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
 }
 
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
