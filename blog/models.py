5c58f593c7d1a3f40139ea651a02b95ae3ac02f0
revalle88 <revalle88@gmail.com>
2019-08-21 00:33:41 +0400

    post image

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.search import index
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
@@ -18,19 +19,17 @@ class BlogIndexPage(Page):
         FieldPanel('intro', classname="full")
     ]
 
-    def serve(self, request):
-        # Get blogs
-        blogs = BlogPage.objects.child_of(self).live()
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        posts = BlogPage.objects.child_of(self).live()
 
         # Filter by tag
         tag = request.GET.get('tag')
         if tag:
-            blogs = blogs.filter(tags__name=tag)
-
-        return render(request, self.template, {
-            'page': self,
-            'blogs': blogs,
-        })
+            posts = posts.filter(tags__name=tag)
+        context['posts'] = posts
+        return context
 
 
 class BlogPageTag(TaggedItemBase):
@@ -41,6 +40,13 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -49,6 +55,7 @@ class BlogPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
+        ImageChooserPanel('main_image'),
         FieldPanel('tags'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
