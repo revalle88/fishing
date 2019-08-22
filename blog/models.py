8ee232fe3fcd90a8f1d3876079cc4d5d94d3c431
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-08-22 16:47:24 +0300

    categories and tags

--- blog/models.py
+++ blog/models.py
@@ -9,69 +9,15 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.search import index
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
 
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
-
-    def get_context(self, request):
-        # Update context to include only published posts, ordered by reverse-chron
-        context = super().get_context(request)
-        posts = BlogPage.objects.child_of(self).live()
-        categories = BlogCategory.objects.all()
-
-        # Filter by tag
-        tag = request.GET.get('tag')
-        if tag:
-            posts = posts.filter(tags__name=tag)
-        context['posts'] = posts
-        context['categories'] = categories
-        return context
-
-
-class BlogPageTag(TaggedItemBase):
-    content_object = ParentalKey('BlogPage', related_name='tagged_items', on_delete=models.CASCADE)
-
-
-class BlogPage(Page):
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
-    main_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
-    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
-
-    content_panels = Page.content_panels + [
-        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
-        FieldPanel('date'),
-        ImageChooserPanel('main_image'),
-        FieldPanel('tags'),
-        FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
-    ]
-
-
 @register_snippet
-class BlogCategory(models.Model):
+class BlogPageCategory(models.Model):
     name = models.CharField(max_length=255)
     icon = models.ForeignKey(
         'wagtailimages.Image', null=True, blank=True,
@@ -96,3 +42,91 @@ class BlogCategory(models.Model):
 
     class Meta:
         verbose_name_plural = 'blog categories'
+
+
+class BlogIndexPage(RoutablePageMixin, Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(BlogIndexPage, self).get_context(request, *args, **kwargs)
+        context['categories'] = BlogPageCategory.objects.all()
+        context['posts'] = self.posts
+        context['blog_page'] = self
+        return context
+
+    def get_posts(self):
+        return BlogPage.objects.descendant_of(self).live()
+
+    @route(r'^tag/(?P<tag>[-\w]+)/$')
+    def post_by_tag(self, request, tag, *args, **kwargs):
+        self.search_type = 'tag'
+        self.search_term = tag
+        self.posts = self.get_posts().filter(tags__slug=tag)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^category/(?P<category>[-\w]+)/$')
+    def post_by_category(self, request, category, *args, **kwargs):
+        self.search_type = 'category'
+        self.search_term = category
+        self.posts = self.get_posts().filter(categories__slug=category)
+        print(len(self.posts))
+        print(str(self.get_posts().filter(categories__slug=category)))
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^$')
+    def post_list(self, request, *args, **kwargs):
+        self.posts = self.get_posts()
+        return Page.serve(self, request, *args, **kwargs)
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPage', related_name='tagged_items', on_delete=models.CASCADE)
+    slug = models.SlugField(
+        verbose_name="slug",
+        allow_unicode=True,
+        max_length=255,
+        help_text='A slug to identify posts by this category',
+        blank=True,
+    )
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField(BlogPageCategory, blank=True)
+
+    @property
+    def blog_page(self):
+        return self.get_parent().specific
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        FieldPanel('date'),
+        ImageChooserPanel('main_image'),
+        FieldPanel('tags'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super(BlogPage, self).get_context(request, *args, **kwargs)
+        context['blog_page'] = self.blog_page
+        return context
