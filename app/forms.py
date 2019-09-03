62ded0ea603e9c4d9280286e2fa54ccfe786a240
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-09-03 15:17:28 +0300

    datefield fixed

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from django import forms
 from .models import Pound, Review
 from django_summernote.widgets import SummernoteWidget
 from bootstrap_datepicker_plus import DatePickerInput
+from django.forms import DateField
 
 
 class PoundForm(forms.ModelForm):
@@ -23,6 +24,7 @@ class ReviewForm(forms.ModelForm):
 
     # pound = forms.ModelChoiceField(queryset=Pound.objects.all(), required=False)
     # rating = forms.IntegerField(initial=5, min_value=1, max_value=5)
+    fishing_date = DateField(input_formats=['%Y-%m-%d', '%m/%d/%Y', '%m/%d/%y'])
 
     class Meta:
         model = Review
@@ -48,5 +50,4 @@ class ReviewForm(forms.ModelForm):
         self.fields["fish_caught"].help_text = ""
         self.fields["lat"].widget = forms.widgets.HiddenInput()
         self.fields["lang"].widget = forms.widgets.HiddenInput()
-        # self.fields["fishing_date"].widget = forms.widgets.SelectDateWidget()
-        self.fields["fishing_date"].widget = DatePickerInput()
+        self.fields["fishing_date"].widget = DatePickerInput(format='%m/%d/%Y')
