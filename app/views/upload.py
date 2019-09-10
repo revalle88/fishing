65bfcbe726ced80b4bc61ec7795a3638b5eb0bde
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-09-10 15:22:30 +0300

    photoes uploading for pounds

--- app/views/upload.py
+++ app/views/upload.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 
 from ..forms import PhotoForm
-from ..models import Photo
 
 
 class BasicUploadView(View):
-    def get(self, request):
-        photos_list = Photo.objects.all()
-        return render(self.request, 'photos/basic_upload/index.html', {'photos': photos_list})
-
     def post(self, request):
         form = PhotoForm(self.request.POST, self.request.FILES)
         if form.is_valid():
